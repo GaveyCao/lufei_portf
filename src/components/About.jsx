733e7e4f2e4ca739e0897208c6bfc236441e6cf8
jiahui cao <caojh1993@gmail.com>
2024-05-16 00:31:01 -0700

    update

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -17,11 +17,10 @@ const About = () => {
         variants={fadeIn("", "", 0.1, 1)}
         className='mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]'
       >
-        I'm a skilled software developer with experience in TypeScript and
-        JavaScript, and expertise in frameworks like React, Node.js, and
-        Three.js. I'm a quick learner and collaborate closely with clients to
-        create efficient, scalable, and user-friendly solutions that solve
-        real-world problems. Let's work together to bring your ideas to life!
+        As a designer, my projects range from web interfaces to mobile apps, 
+        each enhancing my dedication to user-centric design solutions. 
+        My goal is to merge aesthetic elegance with functional design, 
+        enhancing both user interaction and satisfaction.
       </motion.p>
     </>
   );
